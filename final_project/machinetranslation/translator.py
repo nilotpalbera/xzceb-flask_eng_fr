aecd469ff6fc47356ff593addd1dad1c6360302c
nilotpalbera <nilotpalbera@yahoo.co.in>
2022-05-04 08:08:47 +0000

    Temporary changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,20 @@
+'''Code for machine language translation'''
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
