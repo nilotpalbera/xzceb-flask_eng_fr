a1b5a049c893f0fe5eace240bcd0f3ae17b7a77e
nilotpalbera <nilotpalbera@yahoo.co.in>
2022-05-04 10:02:53 +0000

    Temporary changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,29 @@
+import unittest
+from translator import englishToFrench, frenchToEnglish
+
+class TEST_TRANSLATION(unittest.TestCase):
+    def test_cases(self):
+        #en-fr
+        self.assertEqual(englishToFrench('Hi'), 'Salut')
+        self.assertEqual(englishToFrench('English to French'), 'Anglais vers français')
+
+        #fr-en 
+        self.assertEqual(frenchToEnglish('Salut'), 'Hi')
+        self.assertEqual(frenchToEnglish('français vers anglais'), 'French to English')
+
+        #None values
+        try:
+            self.assertEqual(englishToFrench(None), '')
+            self.assertEqual(frenchToEnglish(None), '')
+        except ValueError as err:
+            print(err)
+
+        #en-fr, Hello and Bonjour
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertNotEqual(englishToFrench('Bonjour'), 'Hello')
+
+        #fr-en, Hello and Bonjour
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        self.assertNotEqual(frenchToEnglish('Hello'), 'Bonjour')
+
+unittest.main()
